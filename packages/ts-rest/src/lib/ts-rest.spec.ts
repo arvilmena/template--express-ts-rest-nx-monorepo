9984ccfb6e2cbe758737fc08f33faa4aac791471
Arvil Meña <arvilmena@gmail.com>
2024-01-04 06:07:27 +0800

    nx g @nx/js:library --name=ts-rest --unitTestRunner=jest --bundler=swc --directory=packages/ts-rest --importPath=@stonkerino/ts-rest --publishable=true --projectNameAndRootFormat=as-provided

---
+++ packages/ts-rest/src/lib/ts-rest.spec.ts
@@ -0,0 +1,7 @@
+import { tsRest } from './ts-rest';
+
+describe('tsRest', () => {
+  it('should work', () => {
+    expect(tsRest()).toEqual('ts-rest');
+  });
+});
---
+++ packages/ts-rest/src/lib/ts-rest.ts
@@ -0,0 +1,3 @@
+export function tsRest(): string {
+  return 'ts-rest';
+}
